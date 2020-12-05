4911ab38337c39e55ab8ec1d3179fd52fef2ad38
Garrett Sweeney <garrett.d.sweeney@gmail.com>
2020-12-05 14:28:07 -0500

    Some admin improvements

--- polls/admin.py
+++ polls/admin.py
@@ -4,6 +4,20 @@ from .models import Question
 
 from .models import Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # Adds a filter in admin page for pub date
+    list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
 
 admin.site.register(Choice)
